8798e63578c5d0cd109d80b824cb3f07a5cb4974
Daria_Zaika <dashazaika@gmail.com>
2022-04-29 17:44:42 +0300

    pypi package finish

--- mylib_package/mylib.py
+++ mylib_package/mylib.py
@@ -15,11 +15,15 @@ r_nbu = requests.get(url_nbu)
 df_privat = pd.read_html(r_privat.text)[0]
 df_nbu = pd.read_html(r_nbu.text)[1]
 
-df_priv_chage = df_privat.rename(columns={'Покупка': 'Rate', 'Валюта': 'Currency'})
-df_priv_res = df_priv_chage.loc[df_priv_chage['Currency'] == currencyname, ['Currency', 'Rate']]
 
-df_nbu_chage = df_nbu.rename(columns={'Курс': 'Rate', 'Валюта': 'Currency'})
-df_nbu_res = df_nbu_chage.loc[df_nbu_chage['Currency'] == currencyname, ['Currency', 'Rate']]
+def result():
+    df_priv_chage = df_privat.rename(columns={'Покупка': 'Rate', 'Валюта': 'Currency'})
+    df_nbu_chage = df_nbu.rename(columns={'Курс': 'Rate', 'Валюта': 'Currency'})
+    df_nbu_res = df_nbu_chage.loc[df_nbu_chage['Currency'] == currencyname, ['Currency', 'Rate']]
+    df_priv_res = df_priv_chage.loc[df_priv_chage['Currency'] == currencyname, ['Currency', 'Rate']]
+    print(f"NBU rate:\n{df_nbu_res}")
+    print(f"PrivatBank rate:\n{df_priv_res}")
+
 
 if __name__ == '__main__':
-    print(f"NBU rate:\n{df_nbu_res}")
+    result()
